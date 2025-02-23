0125872977038ffa2a5214c3bfebfd405c60902c
coleakd <coleakd@gmail.com>
2025-02-23 18:19:24 +0300

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,20 @@
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/hero-img.png";
+import themeIcon from "../../assets/sun.svg";
+
 function Hero() {
-  return <div>Some other text</div>;
+  return (
+    <section id="hero" className={styles.Container}>
+      <div className={styles.colorModeContainer}>
+        <img className={styles.hero} src={heroImg} alt="Limz profile picture" />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
