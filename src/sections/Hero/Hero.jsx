7ec78ae8ecc6699b9fbb6c49938e9d62974dbb31
coleakd <coleakd@gmail.com>
2025-02-23 18:38:55 +0300

    Hero Section population
    Populating the hero section

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
 import themeIcon from "../../assets/sun.svg";
+import twitterIcon from "../../assets/twitter-light.svg";
+import githubIcon from "../../assets/github-light.svg";
+import linkedinIcon from "../../assets/linkedin-light.svg";
+import instagramIcon from "../../assets/instagram-light.svg";
 
 function Hero() {
   return (
@@ -12,6 +16,27 @@ function Hero() {
           src={themeIcon}
           alt="Color mode icon"
         />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Limz <br />
+          Diner
+        </h1>
+        <h2>Swahili Dish District</h2>
+        <span>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://instagram.com/" target="_blank">
+            <img src={instagramIcon} alt="Instagram icon" />
+          </a>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
       </div>
     </section>
   );
